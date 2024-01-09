64b5d8ea2de5f42b282d136976eceb8b382f7758
Vishrut Agarwalla <vishrutagarwalla19@gmail.com>
2024-01-10 03:50:20 +0530

    Improved  Responsiveness to All Project Listing Cards

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0fb7f019bd80ddc9"; }
+/******/ 		__webpack_require__.h = function() { return "ba07bb7279602f74"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
